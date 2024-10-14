b39835477ea8e0dfa65d181b3a749ea219e07d4c
Jason Curtis <jason.curtis@plexd.net>
2024-10-14 12:50:02 -0500

    fix: change min/default/max ttl cache policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,9 +94,9 @@ resource "aws_cloudfront_distribution" "slashdev_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
+    min_ttl                = 300
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   price_class = "PriceClass_100"
